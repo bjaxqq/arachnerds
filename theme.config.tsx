026d096e12b712b441798abaeb906a0afdbcc68f
bjaxqq <brooksieee@proton.me>
2024-11-18 21:18:39 -0500

    Update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Weaver's Wrath</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
+    link: 'https://github.com/Arachnerds/SER225',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
 }
 
 export default config
